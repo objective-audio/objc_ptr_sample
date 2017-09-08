949abd93ae46b1e55d7cf776ecf68ac2afc5283d
Yuki Yasoshima <yukiyasos@gmail.com>
2017-09-08 21:38:35 +0900

    Xcodeプロジェクトを作成

---
+++ objc_ptr_sample/objc_ptr_sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc_ptr_sample
+//
+//  Created by Yuki Yasoshima on 2017/09/08.
+//  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
