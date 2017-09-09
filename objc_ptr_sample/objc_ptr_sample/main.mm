a278fad2936ddde972dcf66a49e2a87c6410ae62
Yuki Yasoshima <yukiyasos@gmail.com>
2017-09-09 10:57:22 +0900

    サンプルを追加

--- objc_ptr_sample/objc_ptr_sample/main.mm
+++ objc_ptr_sample/objc_ptr_sample/main.mm
@@ -6,12 +6,177 @@
 //  Copyright © 2017年 Yuki Yasoshima. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "ObjCClass.h"
 
-int main(int argc, const char * argv[]) {
+void sample_construct_with_objc_object() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        TestClass *testObj = [[[TestClass alloc] initWithText:@"construct_with_objc_object"] autorelease];
+
+        objc_ptr<TestClass *> test_ptr(testObj);
+
+        NSLog(@"text : %@", test_ptr.value().text);
+    }
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_make_objc_ptr() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    TestClass *testObj = [[TestClass alloc] initWithText:@"make_objc_ptr"];
+
+    auto test_ptr = make_objc_ptr(testObj);
+
+    NSLog(@"text : %@", test_ptr.value().text);
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_remove_autorelease() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    objc_ptr<TestClass *> test_ptr(
+        []() { return [[[TestClass alloc] initWithText:@"remove_autorelease"] autorelease]; });
+
+    NSLog(@"text : %@", test_ptr.value().text);
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_asterisk() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"asterisk"]);
+
+    NSLog(@"text : %@", [*test_ptr text]);
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_operator_bool() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"operator_bool"]);
+
+    if (test_ptr) {
+        NSLog(@"test_ptr text : %@", test_ptr.value().text);
+    }
+
+    objc_ptr<TestClass *> empty_ptr;
+
+    if (!empty_ptr) {
+        NSLog(@"empty_ptr text : %@", empty_ptr.value().text);
+    }
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_weak_objc_ptr_from_objc_ptr() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    weak_objc_ptr<TestClass *> weak_ptr;
+    {
+        auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"weak_objc_ptr_from_objc_ptr"]);
+
+        weak_ptr = test_ptr;
+
+        auto locked_ptr = weak_ptr.lock();
+
+        NSLog(@"retained text : %@", locked_ptr.value().text);
+    }
+
+    auto locked_ptr = weak_ptr.lock();
+
+    NSLog(@"released text : %@", locked_ptr.value().text);
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_to_weak() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"to_weak"]);
+
+    auto weak_ptr = to_weak(test_ptr);
+
+    auto locked_ptr = weak_ptr.lock();
+
+    NSLog(@"locked text : %@", locked_ptr.value().text);
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_to_weak_with_lambda() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"to_weak_with_lambda"]);
+
+    auto weak_ptr = to_weak(test_ptr);
+
+    auto handler = [weak_ptr]() {
+        auto locked_ptr = weak_ptr.lock();
+
+        NSLog(@"locked text : %@", locked_ptr.value().text);
+    };
+
+    handler();
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_to_weak_with_lambda_capture() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    auto test_ptr = make_objc_ptr([[TestClass alloc] initWithText:@"to_weak_with_lambda_capture"]);
+
+    auto handler = [weak_ptr = to_weak(test_ptr)]() {  // C++14
+        auto locked_ptr = weak_ptr.lock();
+
+        NSLog(@"locked text : %@", locked_ptr.value().text);
+    };
+
+    handler();
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+void sample_instance_var() {
+    NSLog(@"begin : %s", __PRETTY_FUNCTION__);
+
+    ObjCClass *objcObj = [[ObjCClass alloc] init];
+
+    TestClass *testObj = [[TestClass alloc] initWithText:@"private_var"];
+
+    [objcObj setTestObject:testObj];
+
+    NSLog(@"text : %@", [objcObj testObject].text);
+
+    [testObj release];
+
+    [objcObj release];
+
+    NSLog(@"end : %s", __PRETTY_FUNCTION__);
+}
+
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        auto const functions = {sample_construct_with_objc_object,
+                                sample_make_objc_ptr,
+                                sample_remove_autorelease,
+                                sample_asterisk,
+                                sample_operator_bool,
+                                sample_weak_objc_ptr_from_objc_ptr,
+                                sample_to_weak,
+                                sample_to_weak_with_lambda,
+                                sample_instance_var};
+
+        for (auto const &func : functions) {
+            func();
+
+            NSLog(@"");
+        }
     }
     return 0;
 }
